572c3594fdaf58b41d0e300d17c50c18dbc9ae33
Simran Fitzgerald <fitzgeraldsimran@hotmail.com>
2013-05-13 08:26:13 -0400

    minor change to tcp server

--- tcp-server.js
+++ tcp-server.js
@@ -1,12 +1,14 @@
 var net = require('net');
 
 var sockets = [];
+var client = new Object(); // {"address" : ipAddress}
 
 var server  = net.createServer(function(socket) {
 	sockets.push(socket)
 	
 	socket.on('data', function(d) {
 		for(var i=0; i < sockets.length; i++) {
+			//TODO instantiate new cleint object and put into sockets array
 			sockets[i].write(d);
 		}
 	});
